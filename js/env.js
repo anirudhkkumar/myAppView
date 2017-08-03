9dce039d7ac4d4c510a6420f3c8f582e6f2245b0
mgupta <mgupta1@wm.com>
2017-08-03 20:01:47 +0530

    env staging server codedep.in

--- js/env.js
+++ js/env.js
@@ -2,7 +2,7 @@
   window.__env = window.__env || {};
 
   // API url
-  window.__env.apiUrl = 'http://localhost:3000';
+  window.__env.apiUrl = 'http://codedep.in';
 
   // Base url
   window.__env.baseUrl = '/';
