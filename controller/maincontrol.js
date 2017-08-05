d4c536d17bd2025fc8434461480772f8c2437bb8
mgupta <mgupta1@wm.com>
2017-08-05 16:24:36 +0530

    adding correections

--- controller/maincontrol.js
+++ controller/maincontrol.js
@@ -18,7 +18,7 @@ app.controller("logincontrol", function($scope, $http, $window, $document) {
             //console.log(response.data.data);
             localStorage.setItem('sessions', JSON.stringify($scope.usrData.data));
 
-           $window.location.href = "/myprofile";
+           $window.location.href = "/myprofile.html";
         }, function myError(err) {
             $scope.usrData = err.statusText;
             console.log(err);
@@ -38,7 +38,7 @@ app.controller("myProfilecontrol", function($scope, $http, $window, $document) {
     var usrData = localStorage.getItem('sessions');
 
     if(!usrData){
-          $window.location.href = "/login";
+          $window.location.href = "/login.html";
     }
     else{
 
@@ -48,33 +48,8 @@ app.controller("myProfilecontrol", function($scope, $http, $window, $document) {
             method : "POST",
             url : url,
             data: {
-                "loginToken": usrData.loginToken,
-            },
-            method : "POST",
-            url : url,
-            "basic": {
-                "firstName": $scope.firstname,
-                "middleName": $scope.middlename,
-                "lastName": $scope.lastname,
-                "DOB": $scope.dob,
-                "TOB": $scope.tob,
-                "complex": $scope.complex,
-                "disablity": $scope.disablity,
-                "gender": $scope.gender,
-                "aboutMe": $scope.aboutme,
-                "height": $scope.height,
-                "maritialStatus": $scope.martialstatus,
-                "belongsToCountry": $scope.,
-                "belongsToState": ,
-                "belongsToCity": ,
-                "currentToCountry": ,
-                "currentToState": ,
-                "currentToCity": ,
-                "profileManagedBy": ,
-                "languageknown": [
-                    "hindi",
-                    "english"
-                ]
+                "username": usrData.username,
+                "loginToken": usrData.loginToken
             },
             headers: {'Content-Type': 'application/json'}
         }).then(function mySuccess(response) {
@@ -87,9 +62,7 @@ app.controller("myProfilecontrol", function($scope, $http, $window, $document) {
             $scope.myWelcome = err.statusText;
             console.log(err);
         });
-    } 
-    
-    
+    }   
 });
 
 
@@ -108,7 +81,7 @@ function logout_func ($scope, $http, $window, usrData){
             $scope.myWelcome = response.data;
             //console.log(response.data.data);
             localStorage.removeItem('sessions');
-            $window.location.href = "/login";
+            $window.location.href = "/login.html";
         }, function myError(err) {
 
             $scope.myWelcome = err.statusText;
